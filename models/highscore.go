53d98b954aa9b8430764416152fe78d97bac80f4
Riku <kukkaniemi.riku@gmail.com>
2021-12-06 12:30:06 +0200

    Add highscore model

---
+++ models/highscore.go
@@ -0,0 +1,9 @@
+package models
+
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
+type Highscore struct {
+	ID    primitive.ObjectID `bson:"_id"`
+	Name  *string            `json:"name"`
+	Score *int64             `json:"score"`
+}
