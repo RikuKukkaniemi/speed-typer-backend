905f3338324c33c61962cd64aadff25b014edba6
Riku <kukkaniemi.riku@gmail.com>
2021-12-13 09:09:46 +0200

    Refactor GetHighscores to show top 10 scores

--- routes/highscores.go
+++ routes/highscores.go
@@ -2,7 +2,7 @@ package routes
 
 import (
 	"context"
-	"fmt"
+	"log"
 	"net/http"
 	"rikukukkaniemi/speed-typer-backend/models"
 	"time"
@@ -11,6 +11,7 @@ import (
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 var highscoreCollection *mongo.Collection = OpenCollection(Client, "highscores")
@@ -23,7 +24,7 @@ func AddHighscore(c *gin.Context) {
 
 	if err := c.BindJSON(&highscore); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		fmt.Println(err)
+		log.Println(err)
 		return
 	}
 
@@ -34,41 +35,42 @@ func AddHighscore(c *gin.Context) {
 	highscore.ID = primitive.NewObjectID()
 	highscore.Date = primitive.NewDateTimeFromTime(time.Now())
 
-	result, insertErr := highscoreCollection.InsertOne(ctx, highscore)
+	_, insertErr := highscoreCollection.InsertOne(ctx, highscore)
 	if insertErr != nil {
-		msg := fmt.Sprintf("highscore item was not created")
-		c.JSON(http.StatusInternalServerError, gin.H{"error": msg})
-		fmt.Println(insertErr)
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "highscore item was not created"})
+		log.Println(insertErr)
 		return
 	}
 	defer cancel()
 
-	c.JSON(http.StatusOK, result)
+	GetHighscores(c)
 }
 
-//get all highscores
+//get top 10 highscores
 func GetHighscores(c *gin.Context) {
 	var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
 
 	var highscores []bson.M
 
-	cursor, err := highscoreCollection.Find(ctx, bson.M{})
+	findOptions := options.Find()
+	findOptions.SetSort(bson.D{{"score", -1}})
+	findOptions.SetLimit(10)
+
+	cursor, err := highscoreCollection.Find(ctx, bson.M{}, findOptions)
 
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-		fmt.Println(err)
+		log.Println(err)
 		return
 	}
 
 	if err = cursor.All(ctx, &highscores); err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-		fmt.Println(err)
+		log.Println(err)
 		return
 	}
 
 	defer cancel()
-
-	fmt.Println(highscores)
 
 	c.JSON(http.StatusOK, highscores)
 }
